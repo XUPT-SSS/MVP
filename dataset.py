51f631bc2aac9bfe9322a029b698b38473171d00
zhaojiangw <130352645+zhaojiangw@users.noreply.github.com>
2025-10-22 17:40:19 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -6,9 +6,7 @@ train_dataset_path = './train.csv'
 test_dataset_path = './test.csv'  
 
 def load_code_dataset(dataset_path):
-    dataset = []  # 初始化一个空列表来存储数据集
-
-    # 使用pandas库读取CSV文件
+    dataset = [] 
     try:
         df = pd.read_csv(dataset_path)
     except Exception as e:
@@ -17,16 +15,16 @@ def load_code_dataset(dataset_path):
 
     # 遍历数据框的每一行
     for index, row in df.iterrows():
-        code = row['nor_code']  # 从'code'列获取代码片段
-        label = row['label'] - 1  # 从'label'列获取标签
-        cwe_id = row['cwe_id']  # 加载cwe_id
-        # 将代码片段和对应的标签以元组形式添加到数据集中
+        code = row['nor_code']  
+        label = row['label'] - 1  
+        cwe_id = row['cwe_id']  
+
         # dataset.append((code, label))
         dataset.append((code, label,cwe_id))
     return dataset
 class DatasetWithTextLabel(object):
     def __init__(self,split='test'):
-        # 根据数据集名称和拆分（训练、验证或测试）确定数据集路径
+
         if split == 'train':
             dataset_path = train_dataset_path
         elif split == 'test':
@@ -38,20 +36,12 @@ class DatasetWithTextLabel(object):
         code,label,cwe_id = self.dataset[i]
         return code,label,cwe_id
     def __len__(self):  
-        # 返回数据集的长度（样本数量）
         return len(self.dataset)
     def calculate_num_classes(self):
-        # 计算数据集的类别数量
-        # label 存储在数据集的第二列
-        # labels = [label for _, label in self.dataset]
         labels = [label for _,label,_ in self.dataset]
         return len(set(labels)),labels
-# man = DatasetWithTextLabel(split='test')
-# print(man.__len__())
 
-# code, label = man[180]
-# print(f'code:{code}')
-# print(f'label:{label}')
+
 
 
 
